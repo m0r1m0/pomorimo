7ea0d40e71e9c4d26009778fff23dd987968077b
m0r1m0 <yuki.wd7@gmail.com>
2023-06-29 09:49:54 +0900

    feat: add alarm sound

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useInterval } from "./hooks/useInterval";
 
 const FOCUS_DURATION = 0.1 * 60
@@ -10,6 +10,8 @@ export default function Index() {
   const [count, setCount] = useState(FOCUS_DURATION);
   const [isFocusMode, setIsFocusMode] = useState(true);
   const [timerRunning, setTimerRunning] = useState(false);
+  const [alarmPlaying, setAlarmPlaying] = useState(false);
+  const alarmSoundRef = useRef<HTMLAudioElement | null>(null);
 
   useInterval(
     () => {
@@ -22,6 +24,7 @@ export default function Index() {
           setIsFocusMode(true);
         }
         setTimerRunning(false);
+        playAlarmSound();
         return;
       }
       setCount((c) => c - 1);
@@ -29,19 +32,53 @@ export default function Index() {
     timerRunning ? 1000 : null
   );
 
+  const playAlarmSound = () => {
+    if (!alarmSoundRef.current) {
+      const alarmSound = new Audio("alarm-clock-short-6402.mp3");
+      alarmSoundRef.current = alarmSound;
+    }
+    setAlarmPlaying(true);
+    alarmSoundRef.current.loop = true;
+    alarmSoundRef.current.play().catch((error) => {
+      console.error("Failed to play alarm sound:", error);
+      setAlarmPlaying(false);
+    });
+  };
+
+  const stopAlarmSound = () => {
+    if (alarmSoundRef.current !== null) {
+      alarmSoundRef.current.pause();
+      alarmSoundRef.current.currentTime = 0;
+    }
+    setAlarmPlaying(false);
+  };
+
+  const handleClick = () => {
+    if (alarmPlaying) {
+      stopAlarmSound();
+      return;
+    }
+
+    if (timerRunning) {
+      setTimerRunning(false);
+      return;
+    }
+
+    if (!timerRunning) {
+      setTimerRunning(true);
+      return;
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <span className="text-2xl">{isFocusMode ? "FOCUS" : "BREAK"}</span>
       <span className="text-9xl mt-4">{Math.floor(count / 60).toString().padStart(2, '0')}:{(count % 60).toString().padStart(2, '0')}</span>
       <button
         className="mt-8 bg-slate-50 text-black font-bold w-40 h-10 text-2xl rounded"
-        onClick={() => {
-          if (!timerRunning) {
-            setTimerRunning(true);
-          }
-        }}
+        onClick={handleClick}
       >
-        START
+        {alarmPlaying || timerRunning ? "STOP" : "START"}
       </button>
     </main>
   );
