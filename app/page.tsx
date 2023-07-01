bd9922710b7b8cea3f41a2b883f7a0326a604fc0
m0r1m0 <yuki.wd7@gmail.com>
2023-07-01 13:37:59 +0900

    feat: change type of token input field to password

--- app/page.tsx
+++ app/page.tsx
@@ -144,7 +144,7 @@ export default function Index() {
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="token"
-              type="text"
+              type="password"
               placeholder="token"
               onChange={(v) => {
                 setToken(v.target.value);
