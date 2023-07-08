f9e9fee1b1857bb409122e3b4930cbf9ef87e475
m0r1m0 <yuki.wd7@gmail.com>
2023-07-08 17:17:18 +0900

    fix: add (root)/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { Logo } from "./components/Logo";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Pomorimo",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="fixed top-2 flex items-center ml-8">
+          <Logo className="w-10" />
+          <span className="font-bold ml-2 text-2xl">Pomorimo</span>
+        </div>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
